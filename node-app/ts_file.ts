8376c211e23aaadf6a2811e87b2a770ac1cc9afe
starboy <124185505+new-Begginings@users.noreply.github.com>
2024-08-30 00:39:10 +0530

    commit uptill week 9

--- node-app/ts_file.ts
+++ node-app/ts_file.ts
@@ -17,6 +17,72 @@ function is_adult(age: number) {
     }
 }
 
+// functions as args
+
+
+function runafter1s(fn: () => void) {
+    setTimeout(fn, 1000);
+}
+
+runafter1s(() => {
+    console.log("hey suar!")
+})
+
+interface User {
+    name: string,
+    age: number,
+};
+
+ 
+// types and interfaces/
+// types lets us do few more things -> union and intersections
+
+// lets say id can be string or a number
+
+type id = string | number;
+
+function printId(user_id: id) {
+    return user_id;
+}
+
+console.log(printId(123))
+
+// next thing is intersection
+
+type employee = {
+    name: string | boolean,
+    eid: string
+}
+
+type manager = {
+    mid: string
+}
+
+type team_lead = employee & manager;
+
+const team_lead_kirat: team_lead = {
+    name: "kirat",
+    eid: "123",
+    mid: "suar"
+}
+
+interface stud {
+    name: string[],
+}
+
+
+
+interface stud {
+    ids: number, 
+}
+
+
+type max_val_arr= number[];
+
+
+function max_val(arr: max_val_arr){
+    return 5;
+}
 
 const sum_value = sum(1,2);
 console.log(sum_value)
@@ -26,3 +92,5 @@ console.log(is_adult(17))
 
 
 greet("Suar")
+
+
